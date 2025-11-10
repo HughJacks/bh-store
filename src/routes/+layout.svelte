10ece07adb09120652e302e3daafa8161309a3af
HughJacks <jackshug@umich.edu>
2025-11-10 14:31:42 -0500

    current

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { goto } from '$app/navigation';
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Page from './+page.svelte';
 
 	let { children } = $props();
 </script>
@@ -10,19 +11,41 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="p-2  min-h-screen">
-	<div class="flex flex-col gap-4">
-		<div class="h-36 flex items-start justify-between w-full max-w-6xl mx-auto">
-			<h1 class="underline cursor-pointer hover:no-underline" onclick={() => goto('/')}>Techtonic</h1>
-			<h1 class="underline cursor-pointer hover:no-underline">Cart (0)</h1>
+<div class=" text-xs min-h-screen w-full max-w-7xl mx-auto  flex flex-col gap-4 p-5 tracking-tightest leading-tight ">
+		<div class="h-72 grid grid-cols-11 flex-row gap-8">
+			<h1 class="text-sm italic col-span-1 text-right " onclick={() => goto('/')}>Tectonic</h1>
+			<div class="col-span-6 "></div>
+			<p class="font-sans  tracking-tightest leading-tight col-span-2 ">A small studio, based in Detroit, whose work focuses on creating unique and functional products.</p>
+
+			<h1 class="underline cursor-pointer hover:no-underline font-sans col-span-2">Cart (0)</h1>
 		</div>
-	</div>
 		<div class="w-full ">
 			{@render children()}
 		</div>
-	
+
+		<div class="h-72 grid grid-cols-11 flex-row gap-8">
+			<h1 class="text-sm italic col-span-1 text-right " onclick={() => goto('/')}>Information</h1>
+			<div class="col-span-2 font-sans  tracking-tightest leading-tight ">
+				<p class="">Contact</p>
+				<p class="">hello@tectonic.org</p>
+				<p class="">+555-555-5555</p>
+				<p class="mb-4">123 Main St, Detroit, MI 48201</p>
+<p>About</p>
+<p class="font-sans tracking-tightest leading-tight" >Tectonic is the collaborative effort of two designers, Ben Michalsky & Hugh Jacks. They met at the University of Michigan and have been working together since 2025.</p>
+
+
+			</div>
+			<div class="col-span-2 tracking-tightest leading-tight">
+				<p class="font-sans mb-4" >Tectonic offers both retail products and studio services</p>
+				<p class="font-sans " >Collaborators: Nihal Patel, Sam Stein, Ryan Krieger, Andrew Weingarten, Ben Michalsky, Hugh Jacks, Jacob Rader, </p>
+
+			</div>
+			<div class="col-span-2 "></div>
+			<p class="font-sans  tracking-tightest leading-tight col-span-2 ">A small studio, based in Detroit, whose work focuses on creating unique and functional products.</p>
+
+			<h1 class="font-sans col-span-2">Tectonic llc copyright 2025</h1>
+		</div>
+
+
 </div>
 
-<div class="p-2 max-w-6xl mx-auto">
-<div class="">© Techtonic llc</div>
-</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,31 +6,64 @@
 	};
 
 	const images: GalleryImageProps[] = [
-		{ src: '/5.jpg', alt: 'Artisan goods arranged on shelves' },
         { src: '/3.jpg', alt: 'Artisan goods arranged on shelves' },
         { src: '/2.jpg', alt: 'Artisan goods arranged on shelves' },
         { src: '/4.jpg', alt: 'Artisan goods arranged on shelves' },
-
-
+        { src: '/7.jpg', alt: 'Artisan goods arranged on shelves' },
+        { src: '/8.jpg', alt: 'Artisan goods arranged on shelves' },
+        { src: '/9.jpg', alt: 'Artisan goods arranged on shelves' },
+        { src: '/10.jpg', alt: 'Artisan goods arranged on shelves' },
+        { src: '/11.jpg', alt: 'Artisan goods arranged on shelves' },
+        { src: '/12.jpg', alt: 'Artisan goods arranged on shelves' },
+        { src: '/13.jpg', alt: 'Artisan goods arranged on shelves' },
+        { src: '/14.jpg', alt: 'Artisan goods arranged on shelves' },
 
 	];
+
+	let currentIndex = $state(0);
+
+	function nextImage() {
+		currentIndex = (currentIndex + 1) % images.length;
+	}
+
+	function goToImage(index: number) {
+		currentIndex = index;
+	}
 </script>
 
-<div class="w-full max-w-6xl mx-auto flex flex-col gap-4">
-<div class="w-full ">
-    <div class="flex flex-row justify-between pb-2">
-	<h1 class="underline cursor-pointer hover:no-underline" onclick={() => goto('/products/trough')}>Trough</h1>
-    <h1 class="">$400.00</h1>
-</div>
-	<div class="flex flex-row gap-2 h-72 overflow-x-auto overflow-y-hidden [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
+<div class="w-full grid grid-cols-11 gap-8 pb-8">
+    <div class="col-span-1 text-right text-sm italic">
 
-		{#each images as image}
-			{@render GalleryImage(image)}
-		{/each}
-	</div>
-    
-
+	<h1 class=" pb-4" >Trough</h1>
 </div>
+<div class="col-span-2">
+    <p class="font-sans " >A shelf designed to hold and display large format design books.</p>
+</div>
+
+<div class="col-span-4 columns-2 gap-8">
+    <p class="font-sans " >Two pieces of birch join via hardware at a 90 degree angle and intersect with two planes of stainless steel in a friction fit, creating a low to the ground, sturdy, and beautiful display shelf. The current lead time is 4-6 weeks</p>
+</div>
+
+<div class="col-span-2">
+    <p class="font-sans " >60" x 18" x 18"</p>
+    <p class="font-sans " >12.2lbs</p>
+</div>
+
+<div class="col-span-2">
+    <button class="font-sans underline hover:no-underline cursor-pointer">Add to Cart</button>
+
+    <p class="font-sans " >$500.00</p>
+</div>
+
+<div class="col-span-1 text-right flex flex-col gap-1">
+    {#each images as _, index}
+        <div class="cursor-pointer" on:click={() => goToImage(index)}>{index === currentIndex ? 'x' : '0'}</div>
+    {/each}
+</div>
+<div class="col-span-8 cursor-e-resize h-144  overflow-hidden" on:click={nextImage}>
+    <img src={images[currentIndex].src} alt={images[currentIndex].alt} class="w-full h-full object-cover rounded-sm" />
+</div>
+
 </div>
 
 {#snippet GalleryImage(image: GalleryImageProps)}
